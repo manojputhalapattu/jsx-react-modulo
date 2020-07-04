7dd9435dc9bf81daa0c7d086a218d28a0824260e
manoj puthalapattu <58524654+manojputhalapattu@users.noreply.github.com>
2020-07-04 21:51:56 +0530

    "Dynamic-time-color"

--- src/index.js
+++ src/index.js
@@ -6,3 +6,30 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+import React from "react";
+import ReactDom from "react-dom";
+
+const today = new Date();
+const t = today.getHours();
+//const jsp =0;
+let styles = {
+  color: "red"
+};
+let jsp;
+if (t < 12) {
+  jsp = "morning";
+  styles.color = "pink";
+  // color = "pink";
+} else if (t < 18) {
+  jsp = "afternoon";
+  // color = "blue";
+} else if (t > 19) {
+  jsp = "Night ";
+  styles.color = "blue";
+  //color = "yellow";
+}
+
+ReactDom.render(
+  <h1 style={styles}> Good {jsp}</h1>,
+  document.getElementById("root")
+);
